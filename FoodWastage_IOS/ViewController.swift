c8d10df9f0c2916f3370aed8fd0e1bfc8e53dffe
Vu Ngo <ntv1790@gmail.com>
2024-10-30 15:38:07 -0400

    Initial Commit

---
+++ FoodWastage_IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodWastage_IOS
+//
+//  Created by CyberKi on 10/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FoodWastage_IOSTests/FoodWastage_IOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodWastage_IOSTests.swift
+//  FoodWastage_IOSTests
+//
+//  Created by CyberKi on 10/30/24.
+//
+
+import Testing
+@testable import FoodWastage_IOS
+
+struct FoodWastage_IOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
